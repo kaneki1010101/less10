5fec4d8edb9e2bca9ee418423dde593a3fa5919d
kaneki1010101 <sultantitlom@gmail.com>
2022-05-19 10:11:13 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        // nav: true,
+        // dots: true,
+    });
+});
